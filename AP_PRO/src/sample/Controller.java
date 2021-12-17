16a06faa9da5b8dd022702b24c2b8835380761ea
amankumar2304 <88782926+amankumar2304@users.noreply.github.com>
2021-12-17 19:09:08 +0530

    Add files via upload

---
+++ AP_PRO/src/sample/Controller.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Button b1;
+
+    @FXML
+    private Button b2;
+
+    @FXML
+    private Label l1;
+
+    @FXML
+    public void but1(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void but2(ActionEvent event) {
+
+    }
+
+}
